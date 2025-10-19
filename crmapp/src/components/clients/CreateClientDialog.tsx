d6e85e70cf00a206e02473bfbf31884d12d5bb58
eltx88 <euanliew77@gmail.com>
2025-10-19 13:42:00 +0800

    Remove unwanted imports

--- crmapp/src/components/clients/CreateClientDialog.tsx
+++ crmapp/src/components/clients/CreateClientDialog.tsx
@@ -6,7 +6,6 @@ import { Dialog, DialogContent,  DialogHeader,  DialogTitle,  DialogFooter} from
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from '@/components/ui/select';
 import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
